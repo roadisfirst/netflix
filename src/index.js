e5f6566c128f5b84c26aa78df5d5351ff8e2e455
roadisfirst <roadisfirst@gmail.com>
2021-09-23 09:44:58 +0300

    testing setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
