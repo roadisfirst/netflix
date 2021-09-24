77c9e44bbe6495c7a0d24acb83c6c5434a9c3446
roadisfirst <roadisfirst@gmail.com>
2021-09-24 15:59:13 +0300

    dependencies resolved for heroku11

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Home } from './pages/Home';
+import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login';
 import Auth from './pages/Auth/Auth';
 import Logout from './pages/Auth/Logout/Logout';
@@ -24,8 +24,8 @@ const App = props => {
         <Switch>
           <Route path={'/'} exact component={Home} />
           <Route path={'/login'} component={Login} />
-          <Route path="/logout" component={Logout} />
           <Route path={'/auth'} component={Auth} />
+          <Route path="/logout" component={Logout} />
           <Redirect to="/" />
         </Switch>
       </BrowserRouter>
